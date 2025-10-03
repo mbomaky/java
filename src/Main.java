a93e9f8d20a6832cd926ec3e937d7c170cde6dab
mbomaky <konstantynolif@gmail.com>
2025-10-03 13:19:08 +0000

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.stream.Stream;
 
+public class Main {
+    private Boolean isDone;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+        Stream<Integer> stream = Stream.of(1, 2);
     }
+
+    public Boolean getDone(String strongParam, String st) {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+
 }
