70afe14cdc0aafce2f321a463d63fd5e81ff04df
mb <konstantynolif@gmail.com>
2026-03-01 01:34:02 +0200

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,25 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+void main() {
+  Transaction transaction1 = new Transaction("T001", 50000, "flat", LocalDate.of(2025, 1, 5));
+  Transaction transaction2 = new Transaction("T002", 1200, "product", LocalDate.of(2025, 1, 10));
+  Transaction transaction3 = new Transaction("T003", 500, "transport", LocalDate.of(2025, 1, 15));
 
-public class Main {
-  public static void main(String[] args) {
-    Transaction transaction1 = new Transaction("T001", 50000, "flat", LocalDate.of(2025, 1, 5));
-    Transaction transaction2 = new Transaction("T002", 1200, "product", LocalDate.of(2025, 1, 10));
-    Transaction transaction3 = new Transaction("T003", 500, "transport", LocalDate.of(2025, 1, 15));
+  List<Transaction> transactions = new ArrayList<>(List.of(transaction1, transaction2, transaction3));
 
-    List<Transaction> transactions = new ArrayList<>(List.of(transaction1, transaction2, transaction3));
+  TransactionAnalyzer transactionAnalyzer = new TransactionAnalyzer();
+  IO.println("Total amount is " + transactionAnalyzer.calculateBalance(transactions));
+  IO.println(
+      "Max transaction is " +
+          transactionAnalyzer.findLargestTransaction(transactions).id() +
+          " (" +
+          transactionAnalyzer.findLargestTransaction(transactions).amount() +
+          ")"
+  );
 
-    TransactionAnalyzer transactionAnalyzer = new TransactionAnalyzer();
-    System.out.println("Total amount is " + transactionAnalyzer.calculateBalance(transactions));
-    System.out.println(
-        "Max transaction is " +
-            transactionAnalyzer.findLargestTransaction(transactions).id() +
-            " (" +
-            transactionAnalyzer.findLargestTransaction(transactions).amount() +
-            ")"
-    );
+  List<Transaction> byCategory = transactionAnalyzer.getTransactionsByCategory(transactions, "product");
+  IO.println("Transaction category \"" + byCategory.getFirst().category() + "\": ");
 
-    List<Transaction> byCategory = transactionAnalyzer.getTransactionsByCategory(transactions, "product");
-    System.out.println("Transaction category \"" + byCategory.getFirst().category() + "\": ");
-
-    String ids = byCategory.stream()
-        .map(Transaction::id)
-        .collect(Collectors.joining(", "));
-    System.out.println(ids);
-  }
+  String ids = byCategory.stream()
+      .map(Transaction::id)
+      .collect(Collectors.joining(", "));
+  IO.println(ids);
 }
--- src/TransactionAnalyzer.java
+++ src/TransactionAnalyzer.java
@@ -1,19 +1,18 @@
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Objects;
 
 public class TransactionAnalyzer {
-  public double calculateBalance(@NotNull List<Transaction> transactions) {
+  public double calculateBalance(List<Transaction> transactions) {
     return transactions.stream().mapToDouble(Transaction::amount).sum();
   }
 
-  public Transaction findLargestTransaction(@NotNull List<Transaction> transactions) {
+  public Transaction findLargestTransaction(List<Transaction> transactions) {
     double maxTransaction = transactions.stream().mapToDouble(Transaction::amount).max().orElse(0.0);
     return transactions.stream().filter(t -> t.amount() == maxTransaction).findFirst().orElse(null);
   }
 
-  public List<Transaction> getTransactionsByCategory(@NotNull List<Transaction> transactions, String category) {
+  public List<Transaction> getTransactionsByCategory(List<Transaction> transactions, String category) {
     return transactions.stream().filter(t -> Objects.equals(t.category(), category)).toList();
   }
 }
